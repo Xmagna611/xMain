afe8ca9663b0a12d044401ef2da375871889e55e
xMagna <103674123+Xmagna611@users.noreply.github.com>
2022-05-17 22:04:35 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'xmagna611',
+  instagram: 'xmagna611'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+      userLoc.textContent = data.location
+    })
+}
+
+getGitHubProfileInfos()
